11572aafaaea42fb50018314ef43a482047fde15
dev-rohit0 <93703830+dev-rohit0@users.noreply.github.com>
2022-01-07 16:19:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+fetch('https://covid-api.mmediagroup.fr/v1//cases?country=India')
+.then(response => response.json())
+.then(data => {
+    console.log(data)
+     document.getElementById('cases').innerHTML = data.All.confirmed.toLocaleString();
+    document.getElementById('deaths').innerHTML = data.All.deaths.toLocaleString();
+    document.getElementById('population').innerHTML = data.All.population.toLocaleString();
+
+});
+fetch('https://covid-api.mmediagroup.fr/v1//vaccines?country=India')
+.then(response => response.json())
+.then(data => {
+    
+     document.getElementById('vaccinations').innerHTML = data.All.people_vaccinated.toLocaleString();
+    document.getElementById('partially').innerHTML = data.All.people_partially_vaccinated.toLocaleString();
+});
