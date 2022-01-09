431f2e6a614ab60c2709c907683864fe5240f065
dev-rohit0 <93703830+dev-rohit0@users.noreply.github.com>
2022-01-09 08:22:10 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 fetch('https://covid-api.mmediagroup.fr/v1//cases?country=India')
 .then(response => response.json())
 .then(data => {
-    console.log(data)
      document.getElementById('cases').innerHTML = data.All.confirmed.toLocaleString();
     document.getElementById('deaths').innerHTML = data.All.deaths.toLocaleString();
     document.getElementById('population').innerHTML = data.All.population.toLocaleString();
