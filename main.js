c1530a476fe64016466de0aa7292baba59b1fd6d
dev-rohit0 <93703830+dev-rohit0@users.noreply.github.com>
2022-02-03 16:29:46 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,65 @@
+let state = document.getElementById('stateName');
+
+document.getElementById('btn').addEventListener('click',stateCase);
+
 fetch('https://covid-api.mmediagroup.fr/v1//cases?country=India')
 .then(response => response.json())
 .then(data => {
-     document.getElementById('cases').innerHTML = data.All.confirmed.toLocaleString();
+   console.log(data);
+   document.getElementById('cases').innerHTML = data.All.confirmed.toLocaleString();
     document.getElementById('deaths').innerHTML = data.All.deaths.toLocaleString();
     document.getElementById('population').innerHTML = data.All.population.toLocaleString();
+    document.getElementById('lastUpdated').style.display = 'none';
 
 });
 fetch('https://covid-api.mmediagroup.fr/v1//vaccines?country=India')
 .then(response => response.json())
 .then(data => {
-    
-     document.getElementById('vaccinations').innerHTML = data.All.people_vaccinated.toLocaleString();
-    document.getElementById('partially').innerHTML = data.All.people_partially_vaccinated.toLocaleString();
+ 
+document.getElementById('vaccinations').innerHTML = data.All.people_vaccinated.toLocaleString();
+document.getElementById('partially').innerHTML = data.All.people_partially_vaccinated.toLocaleString(); 
 });
+
+
+function stateCase(){
+    let stateName = state.value;
+    console.log(stateName);
+
+    if (stateName != "") {
+       
+       fetch('https://covid-api.mmediagroup.fr/v1//cases?country=India')
+    .then(response => response.json())
+    .then(data => {
+       console.log(data);
+           document.getElementById('cases').innerHTML = data[stateName]['confirmed'];
+        document.getElementById('deaths').innerHTML = data[stateName]['deaths'];
+        document.getElementById('population').style.display = 'none';
+        document.getElementById('vaccine').style.display = 'none';
+       document.getElementById('pop').style.display = 'none';
+       document.getElementById('lastUpdated').innerHTML = data[stateName]['updated']; 
+       
+   });
+    } else {
+       
+       fetch('https://covid-api.mmediagroup.fr/v1//cases?country=India')
+        .then(response => response.json())
+        .then(data => {
+           console.log(data);
+           document.getElementById('cases').innerHTML = data.All.confirmed.toLocaleString();
+            document.getElementById('deaths').innerHTML = data.All.deaths.toLocaleString();
+            document.getElementById('population').innerHTML = data.All.population.toLocaleString();
+     
+       });
+       fetch('https://covid-api.mmediagroup.fr/v1//vaccines?country=India')
+        .then(response => response.json())
+        .then(data => {
+         
+       document.getElementById('vaccinations').innerHTML = data.All.people_vaccinated.toLocaleString();
+       document.getElementById('partially').innerHTML = data.All.people_partially_vaccinated.toLocaleString(); 
+       });
+    }
+
+
+} 
+
+
